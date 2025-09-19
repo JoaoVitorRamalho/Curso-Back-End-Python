7b9833a48173533c2790fe9afdceb9ec84189433
JOAO VITOR RAMALHO GONCALVES <0048613@IFMG.EDU.BR>
2025-09-18 21:21:26 -0300

    Comecando o curso de python

---
+++ aula.py
@@ -0,0 +1,9 @@
+a = input('Teste jegue ')
+print('Olá {}, oque você digitou é uma palavra? {}'.format(a,a.isalpha()))
+x = 1
+y = 2
+z = x + y
+print (z)
+idade=12
+nome='Karla'
+print('Olá {}, você tem {} anos?'.format(nome,idade))
